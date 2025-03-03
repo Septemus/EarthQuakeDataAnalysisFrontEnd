8f2d1ae1e749e58b725db8669ddb1fb9795b618d
joe <musketeerdt@gmail.com>
2025-03-03 15:34:24 +0800

    rid unused fs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import fs from "fs";
 import { exec } from "child_process";
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
