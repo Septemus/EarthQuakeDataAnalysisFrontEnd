f4562510b89539c7060672fd05dbcac4dfa78d3d
joe <musketeerdt@gmail.com>
2025-03-03 15:28:02 +0800

    write bundle plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import fs from "fs";
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
@@ -10,7 +11,25 @@ export default ({ mode }: { mode: string }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      {
+        name: "copy-index-html",
+        apply: "build", // this executes after build. I hope this help
+        writeBundle() {
+          console.log("copying index.html file to templates!📖");
+          fs.copyFileSync(
+            path.resolve(__dirname, "dist/index.html"),
+            path.resolve(__dirname, "../templates/earthquake/index.html")
+          );
+          console.log("copying assets to static!📦");
+          fs.copyFileSync(
+            path.resolve(__dirname, "dist/assets/*"),
+            path.resolve(__dirname, "../static/earthquake/")
+          );
+        },
+      },
+    ],
     server: {
       allowedHosts: [process.env.VITE_HOST_NAME ?? ""],
       // proxy: {
