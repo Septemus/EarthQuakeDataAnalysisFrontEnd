0358cc1f894da1ecf35e14907cdfaf299018a70c
joe <musketeerdt@gmail.com>
2025-03-03 14:33:35 +0800

    auto pack to server for vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-
 // https://vite.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  plugins: [vue()],
-  server: {
-    allowedHosts:["0cc4-103-97-2-67.ngrok-free.app","6851-160-30-128-130.ngrok-free.app"],
-    proxy: {
-      "/api": {
-        target: "https://490f-103-97-2-71.ngrok-free.app",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }: { mode: string }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-});
+    plugins: [vue()],
+    server: {
+      allowedHosts: [process.env.VITE_HOST_NAME ?? ""],
+      // proxy: {
+      //   "/api": {
+      //     target: "https://490f-103-97-2-71.ngrok-free.app",
+      //     changeOrigin: true,
+      //     secure: false,
+      //     rewrite: (path) => path.replace(/^\/api/, ""),
+      //   },
+      // },
+    },
+  });
+};
