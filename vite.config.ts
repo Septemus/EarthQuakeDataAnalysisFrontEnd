cd16ef537008b2367d35a1485dacef5489d0a788
joe <musketeerdt@gmail.com>
2025-03-10 22:02:41 +0800

    remove unused fs module

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { exec } from "child_process";
-import fs from "fs";
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
