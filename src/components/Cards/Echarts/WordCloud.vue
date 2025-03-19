dd30056e4d8218693e5b6cbbc6422b07f969c822
joe <musketeerdt@gmail.com>
2025-03-19 15:01:13 +0800

    extract word cloud

--- src/components/Cards/Echarts/WordCloud.vue
+++ src/components/Cards/Echarts/WordCloud.vue
@@ -1,87 +1,65 @@
 <template>
-    <v-chart class="chart" :option="option" :update-options="{ notMerge: true }" :loading="isLoading" autoresize />
+  <v-chart class="chart" :option="option" :update-options="{ notMerge: true }" :loading="isLoading" autoresize />
 </template>
 <script setup lang="ts">
 // import provincesJson from "@/assets/provinces.json";
 import VChart from "vue-echarts";
-import { ref,onMounted } from "vue";
+import { computed, ref } from "vue";
 import { use } from 'echarts/core'
 import {
-    TooltipComponent,
-    TitleComponent,
+  TooltipComponent,
+  TitleComponent,
 } from 'echarts/components'
 import { CanvasRenderer } from 'echarts/renderers'
+const props = defineProps<{ isLoading: boolean, data: { name: string, value: number }[] }>()
 use([
-    TooltipComponent,
-    TitleComponent,
-    CanvasRenderer,
+  TooltipComponent,
+  TitleComponent,
+  CanvasRenderer,
 ])
-const isLoading = ref<boolean>(true);
-const option = ref({
+const option = computed(() => {
+  return {
     title: {
-        text: '关键词',
-        show: false
+      text: '关键词',
+      show: false
     },
     tooltip: {},
     series: [{
-        type: 'wordCloud',
-        shape: 'circle',
-        left: 'center',
-        top: 'center',
-        width: '100%',
-        height: '100%',
-        right: null,
-        bottom: null,
-        sizeRange: [5, 180],
-        // rotationRange: [-45, 45],
-        gridSize: 12,
-        drawOutOfBound: true,
-        // 这是全局的文字样式，相对应的还可以对每个词设置字体样式
-        textStyle: {
-            fontFamily: 'sans-serif',
-            fontWeight: 'bold',
-            // 颜色可以用一个函数来返回字符串
-            color: function () {
-                // Random color
-                return 'rgb(' + [
-                    Math.round(Math.random() * 160),
-                    Math.round(Math.random() * 160),
-                    Math.round(Math.random() * 160)
-                ].join(',') + ')';
-            },
-            emphasis: {
-                shadowBlur: 2,
-                shadowColor: '#333'
-            }
+      type: 'wordCloud',
+      shape: 'circle',
+      left: 'center',
+      top: 'center',
+      width: '100%',
+      height: '100%',
+      right: null,
+      bottom: null,
+      sizeRange: [5, 180],
+      // rotationRange: [-45, 45],
+      gridSize: 12,
+      drawOutOfBound: true,
+      // 这是全局的文字样式，相对应的还可以对每个词设置字体样式
+      textStyle: {
+        fontFamily: 'sans-serif',
+        fontWeight: 'bold',
+        // 颜色可以用一个函数来返回字符串
+        color: function () {
+          // Random color
+          return 'rgb(' + [
+            Math.round(Math.random() * 160),
+            Math.round(Math.random() * 160),
+            Math.round(Math.random() * 160)
+          ].join(',') + ')';
         },
-        // Data is an array. Each array item must have name and value property.
-        data: []
+        emphasis: {
+          shadowBlur: 2,
+          shadowColor: '#333'
+        }
+      },
+      // Data is an array. Each array item must have name and value property.
+      data: props.data
 
     }]
+  }
 })
-onMounted(() => {
-    fetch(`${import.meta.env.VITE_HOST_NAME}/earthquake/api/locationly_count`, {
-    headers: {
-      "ngrok-skip-browser-warning": "true",
-    },
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      const tmp = Object.entries(res).map((e:any) => {
-        let name=e[0];
-        if(name==="新疆维吾尔自治区") {
-          name="新疆"
-        }
-        return {
-          name,
-          value: e[1],
-        };
-      });
-      console.log(tmp);
-      (option.value.series as any)[0].data=tmp
-      isLoading.value = false;
-    });
-})
+
 </script>
--- src/components/Cards/ProvinceWordCloud.vue
+++ src/components/Cards/ProvinceWordCloud.vue
@@ -22,10 +22,38 @@
       </div>
     </div>
     <div class="block w-full overflow-x-auto flex-grow h-600-px">
-      <WordCloud class=""/>
+      <WordCloud :data="data" :is-loading="isLoading" class=""/>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
+import { onMounted, ref } from 'vue';
 import WordCloud from './Echarts/WordCloud.vue';
+const data=ref<{name:string,value:number}[]>([])
+const isLoading=ref<boolean>(true)
+onMounted(() => {
+    fetch(`${import.meta.env.VITE_HOST_NAME}/earthquake/api/locationly_count`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+    },
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      const tmp = Object.entries(res).map((e:any) => {
+        let name=e[0];
+        if(name==="新疆维吾尔自治区") {
+          name="新疆"
+        }
+        return {
+          name,
+          value: e[1],
+        };
+      });
+      debugger
+      data.value=tmp
+      isLoading.value = false;
+    });
+})
 </script>
