3653e345ad4e1ff3a7926abea8b2cfb622e95bff
joe <musketeerdt@gmail.com>
2025-03-19 15:14:04 +0800

    add province word cloud

--- src/components/admin/LocationAnalysis.vue
+++ src/components/admin/LocationAnalysis.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="flex flex-wrap mt-4">
-    <div class="w-full px-4 content">
+    <div class="w-full mb-12 px-4 content">
       <!-- <h2 class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white">ehjejh</h2> -->
       <ChinaHeatMap class="relative w-full shadow-lg rounded bg-blueGray-700 "/>
+    </div>
+    <div class="w-full mb-12 px-4">
+      <ProvinceWordCloud />
     </div>
   </div>
 </template>
 <script lang="ts">
 import ChinaHeatMap from "../Maps/ChinaHeatMap.vue";
-
+import ProvinceWordCloud from "@/components/Cards/ProvinceWordCloud.vue";
 export default {
   components: {
-    ChinaHeatMap
+    ChinaHeatMap,
+    ProvinceWordCloud
   },
 };
 </script>
