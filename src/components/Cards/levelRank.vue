33e183b9f76eda261ead0b6f9601f663605b318f
joe <musketeerdt@gmail.com>
2025-03-19 22:10:54 +0800

    level rank data

--- src/components/Cards/levelRank.vue
+++ src/components/Cards/levelRank.vue
@@ -1,65 +1,34 @@
 <template>
     <el-table :data="tableData" stripe :table-layout="'fixed'" :height="'100%'" :row-style="{height:'10%'}">
-        <el-table-column prop="date" label="时间" />
-        <el-table-column prop="name" label="地点" />
-        <el-table-column prop="address" label="强度" />
+        <el-table-column prop="occurTime" label="时间" />
+        <el-table-column prop="location" label="地点" />
+        <el-table-column prop="level" label="强度" />
+        <el-table-column prop="depth" label="深度" />
     </el-table>
 </template>
 <script lang="ts" setup>
 import {ElTable as ElTable,ElTableColumn} from "element-plus"
-import { ref } from 'vue';
-const tableData=ref([
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-])
+import { onMounted, ref } from 'vue';
+interface Record{
+  occurTime:string
+  depth:number
+  level:number
+  location:string
+}
+onMounted(()=>{
+  debugger
+  fetch(`${import.meta.env.VITE_HOST_NAME}/earthquake/api/?orderby=level&limit=10`, {
+    headers: {
+      "ngrok-skip-browser-warning": "true",
+    },
+  })
+  .then(res=>res.json())
+  .then((res:Record[])=>{
+    console.log(res)
+    tableData.value=res
+  })
+})
+const tableData=ref<Record[]>([])
 </script>
 <style lang="scss" scoped>
 :deep(.el-scrollbar__view) {
