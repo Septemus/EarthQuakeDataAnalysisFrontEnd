683a09f067c5f3210f045ba06af1573a1d974f21
joe <musketeerdt@gmail.com>
2025-04-03 22:36:40 +0800

    extract table

---
+++ src/components/Cards/Echarts/Table.vue
@@ -0,0 +1,17 @@
+<template>
+    <el-table :data="props.tableData" stripe :table-layout="'fixed'" :height="'100%'" :row-style="{ height: `${100/props.tableData.length}%` }">
+        <el-table-column v-for="c in columns" :prop="c.prop" :label="c.label" />
+    </el-table>
+</template>
+<script lang="ts" setup>
+import { ElTable as ElTable, ElTableColumn } from "element-plus"
+const props = defineProps<{ tableData: { [key: string]: number | string }[], columns: { prop: string, label: string }[] }>()
+</script>
+<style lang="scss" scoped>
+:deep(.el-scrollbar__view) {
+    .el-table__body {
+        height: 100%;
+    }
+    height: 100%;
+}
+</style>
