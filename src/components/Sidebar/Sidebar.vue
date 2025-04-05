db3a69b38ca9ff0798ef88a24ca9b80a1725154f
joe <musketeerdt@gmail.com>
2025-04-05 14:11:42 +0800

    side bar forecast hight light fix

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,23 +1,18 @@
 <template>
   <nav
-    class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6"
-  >
+    class="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
     <div
-      class="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto"
-    >
+      class="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
       <!-- Toggler -->
       <button
         class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-        type="button"
-        v-on:click="toggleCollapseShow('bg-white m-2 py-3 px-6')"
-      >
+        type="button" v-on:click="toggleCollapseShow('bg-white m-2 py-3 px-6')">
         <i class="fas fa-bars"></i>
       </button>
       <!-- Brand -->
       <router-link
         class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-        to="/"
-      >
+        to="/">
         基于spark的地震数据分析平台
       </router-link>
       <!-- User -->
@@ -32,27 +27,21 @@
       <!-- Collapse -->
       <div
         class="md:flex md:flex-col md:items-stretch md:opacity-100 md:relative md:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded"
-        v-bind:class="collapseShow"
-      >
+        v-bind:class="collapseShow">
         <!-- Collapse header -->
-        <div
-          class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200"
-        >
+        <div class="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
           <div class="flex flex-wrap">
             <div class="w-6/12">
               <router-link
                 class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
-                to="/"
-              >
+                to="/">
                 Vue Notus
               </router-link>
             </div>
             <div class="w-6/12 flex justify-end">
-              <button
-                type="button"
+              <button type="button"
                 class="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-                v-on:click="toggleCollapseShow('hidden')"
-              >
+                v-on:click="toggleCollapseShow('hidden')">
                 <i class="fas fa-times"></i>
               </button>
             </div>
@@ -61,92 +50,54 @@
         <!-- Form -->
         <form class="mt-6 mb-4 md:hidden">
           <div class="mb-3 pt-0">
-            <input
-              type="text"
-              placeholder="Search"
-              class="border-0 px-3 py-2 h-12 border border-solid border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
-            />
+            <input type="text" placeholder="Search"
+              class="border-0 px-3 py-2 h-12 border border-solid border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal" />
           </div>
         </form>
 
         <!-- Divider -->
         <hr class="my-4 md:min-w-full" />
         <!-- Heading -->
-        <h6
-          class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline"
-        >
+        <h6 class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
           功能导航
         </h6>
         <!-- Navigation -->
 
         <ul class="md:flex-col md:min-w-full flex flex-col list-none">
           <li class="items-center">
-            <router-link
-              to="/admin/dashboard"
-              v-slot="{ href, navigate, isActive }"
-            >
-              <a
-                :href="href"
-                @click="navigate"
-                class="text-xs uppercase py-3 font-bold block"
-                :class="[
-                  isActive
-                    ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-blueGray-700 hover:text-blueGray-500',
-                ]"
-              >
-                <i
-                  class="fas fa-tv mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
-                ></i>
+            <router-link to="/admin/dashboard" v-slot="{ href, navigate, isActive }">
+              <a :href="href" @click="navigate" class="text-xs uppercase py-3 font-bold block" :class="[
+                isActive
+                  ? 'text-emerald-500 hover:text-emerald-600'
+                  : 'text-blueGray-700 hover:text-blueGray-500',
+              ]">
+                <i class="fas fa-tv mr-2 text-sm" :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"></i>
                 首页
               </a>
             </router-link>
           </li>
 
           <li class="items-center">
-            <router-link
-              to="/admin/location-analysis"
-              v-slot="{ href, navigate, isActive }"
-            >
-              <a
-                :href="href"
-                @click="navigate"
-                class="text-xs uppercase py-3 font-bold block"
-                :class="[
-                  isActive
-                    ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-blueGray-700 hover:text-blueGray-500',
-                ]"
-              >
-                <i
-                  class="fas fa-tools mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
-                ></i>
+            <router-link to="/admin/location-analysis" v-slot="{ href, navigate, isActive }">
+              <a :href="href" @click="navigate" class="text-xs uppercase py-3 font-bold block" :class="[
+                isActive
+                  ? 'text-emerald-500 hover:text-emerald-600'
+                  : 'text-blueGray-700 hover:text-blueGray-500',
+              ]">
+                <i class="fas fa-tools mr-2 text-sm" :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"></i>
                 地区分析
               </a>
             </router-link>
           </li>
 
           <li class="items-center">
-            <router-link
-              to="/admin/time-analysis"
-              v-slot="{ href, navigate, isActive }"
-            >
-              <a
-                :href="href"
-                @click="navigate"
-                class="text-xs uppercase py-3 font-bold block"
-                :class="[
-                  isActive
-                    ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-blueGray-700 hover:text-blueGray-500',
-                ]"
-              >
-                <i
-                  class="fas fa-table mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
-                ></i>
+            <router-link to="/admin/time-analysis" v-slot="{ href, navigate, isActive }">
+              <a :href="href" @click="navigate" class="text-xs uppercase py-3 font-bold block" :class="[
+                isActive
+                  ? 'text-emerald-500 hover:text-emerald-600'
+                  : 'text-blueGray-700 hover:text-blueGray-500',
+              ]">
+                <i class="fas fa-table mr-2 text-sm" :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"></i>
                 时间分析
               </a>
             </router-link>
@@ -154,19 +105,12 @@
 
           <li class="items-center">
             <router-link to="/admin/level-analysis" v-slot="{ navigate, isActive }">
-              <a
-                @click="navigate"
-                class="text-xs uppercase py-3 font-bold block"
-                :class="[
-                  isActive
-                    ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-blueGray-700 hover:text-blueGray-500',
-                ]"
-              >
-                <i
-                  class="fas fa-map-marked mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
-                ></i>
+              <a @click="navigate" class="text-xs uppercase py-3 font-bold block" :class="[
+                isActive
+                  ? 'text-emerald-500 hover:text-emerald-600'
+                  : 'text-blueGray-700 hover:text-blueGray-500',
+              ]">
+                <i class="fas fa-map-marked mr-2 text-sm" :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"></i>
                 强度分析
               </a>
             </router-link>
@@ -174,19 +118,12 @@
 
           <li class="items-center">
             <router-link to="/admin/depth-analysis" v-slot="{ navigate, isActive }">
-              <a
-                @click="navigate"
-                class="text-xs uppercase py-3 font-bold block"
-                :class="[
-                  isActive
-                    ? 'text-emerald-500 hover:text-emerald-600'
-                    : 'text-blueGray-700 hover:text-blueGray-500',
-                ]"
-              >
-                <i
-                  class="fas fa-area-chart mr-2 text-sm"
-                  :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
-                ></i>
+              <a @click="navigate" class="text-xs uppercase py-3 font-bold block" :class="[
+                isActive
+                  ? 'text-emerald-500 hover:text-emerald-600'
+                  : 'text-blueGray-700 hover:text-blueGray-500',
+              ]">
+                <i class="fas fa-area-chart mr-2 text-sm" :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"></i>
                 深度分析
               </a>
             </router-link>
@@ -200,12 +137,16 @@
 
         <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
           <li class="items-center">
-            <router-link
-              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-              to="/admin/forecast"
-            >
-              <i class="fas fa-fingerprint text-blueGray-300 mr-2 text-sm"></i>
-              预测分析
+            <router-link v-slot="{ navigate,isActive }" to="/admin/forecast">
+              <a class="text-xs uppercase py-3 font-bold block" @click="navigate"
+                href="javascript:void(0);" :class="[
+                  isActive
+                    ? 'text-emerald-500 hover:text-emerald-600'
+                    : 'text-blueGray-700 hover:text-blueGray-500',
+                ]">
+                <i class="fas fa-fingerprint text-blueGray-300 mr-2 text-sm"></i>
+                预测分析
+              </a>
             </router-link>
           </li>
 
@@ -217,10 +158,8 @@
 
         <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
           <li class="items-center">
-            <a
-              class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-              :href="`${prefix}/admin`"
-            >
+            <a class="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
+              :href="`${prefix}/admin`">
               <i class="fas fa-newspaper text-blueGray-300 mr-2 text-sm"></i>
               后台管理
             </a>
@@ -243,11 +182,11 @@ export default {
   data() {
     return {
       collapseShow: "hidden",
-      prefix:'',
+      prefix: '',
     };
   },
   methods: {
-    toggleCollapseShow: function (classes:any) {
+    toggleCollapseShow: function (classes: any) {
       this.collapseShow = classes;
     },
   },
@@ -256,7 +195,7 @@ export default {
     UserDropdown,
   },
   mounted() {
-      this.prefix=import.meta.env.VITE_HOST_NAME
+    this.prefix = import.meta.env.VITE_HOST_NAME
   },
 };
 </script>
